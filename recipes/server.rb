7767e5446eebfc3a1c9a381b22833fd17d7393b7
Andrea Campi <andrea.campi@zephirworks.com>
2012-06-05 23:57:37 +0200

    Update the upstart config file for cloud_controller to match the new repo checkout; while here, run CC as the non-privileged user.

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,6 +1,6 @@
 ruby_path    = File.join(rbenv_root, "versions", node.cloudfoundry_common.ruby_1_9_2_version, "bin")
 config_file  = File.join(node.cloudfoundry_common.config_dir, "cloud_controller.yml")
-
+install_path = File.join(node['cloudfoundry_cloud_controller']['vcap']['install_path'], "cloud_controller")
 
 # Needed because the CloudController Gemfile depends on mysql
 include_recipe "mysql::client"
@@ -19,13 +19,16 @@ package "sqlite3"
 package "libsqlite3-dev"
 
 cloudfoundry_component "cloud_controller" do
+  install_path install_path
+  bin_file File.join(install_path, "bin", "cloud_controller")
   pid_file node.cloudfoundry_cloud_controller.server.pid_file
   log_file node.cloudfoundry_cloud_controller.server.log_file
+  upstart_file_cookbook "cloudfoundry-cloud_controller"
 end
 
 bash "run cloudfoundry migrations" do
   user node[:cloudfoundry_common][:user]
-  cwd  File.join(node[:cloudfoundry_common][:vcap][:install_path], "cloud_controller")
+  cwd  install_path
   code "PATH='#{ruby_path}:$PATH' #{File.join(ruby_path, "bundle")} exec rake db:migrate RAILS_ENV=production CLOUD_CONTROLLER_CONFIG='#{config_file}'"
   subscribes :run, resources(:git => node[:cloudfoundry_common][:vcap][:install_path])
   action :nothing
