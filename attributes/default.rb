250777521fc8844299f291dc60a2aa1752e5723b
Andrea Campi <andrea.campi@zephirworks.com>
2012-08-10 18:28:52 +0200

    Bump cloud_controller ref for bug fixes.

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,7 +8,7 @@ default['cloudfoundry_cloud_controller']['vcap']['repo']         = "https://gith
 
 # Git reference to use when fetching the CloudFoundry code. Can be
 # either a specific sha or a reference such as `HEAD` or `master`.
-default['cloudfoundry_cloud_controller']['vcap']['reference']    = "2e7d91bb9130e18c86359e33e02fb5feddceed8b"
+default['cloudfoundry_cloud_controller']['vcap']['reference']    = "bdfd70bf189b188d1f84f0a1290f41adbe610497"
 
 # TODO (trotter): Find out what this does.
 default['cloudfoundry_cloud_controller']['staging_cache_dir'] = "/var/vcap/data/cloud_controller/staging"
