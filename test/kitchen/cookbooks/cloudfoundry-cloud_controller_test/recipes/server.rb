084109693d637e1ff6494be9324535043c4d981e
Andrea Campi <andrea.campi@zephirworks.com>
2012-11-11 23:54:14 +0100

    Add tests for the server recipe.

---
+++ test/kitchen/cookbooks/cloudfoundry-cloud_controller_test/recipes/server.rb
@@ -0,0 +1,21 @@
+include_recipe "postgresql::server"
+include_recipe "cloudfoundry-cloud_controller::database"
+include_recipe "nats-server"
+
+chef_gem "cfoundry"
+
+redis = node['redisio']
+redis_version = '2.2.15'
+location = "#{redis['mirror']}/#{redis['base_name']}#{redis_version}.#{redis['artifact_type']}"
+servers = [{'port' => '5454'}]
+
+redisio_install "vcap_redis" do
+  version           redis_version
+  download_url      location
+  default_settings  redis['default_settings']
+  servers           servers
+end
+
+redisio_service 5454 do
+  action [:enable,:start]
+end
