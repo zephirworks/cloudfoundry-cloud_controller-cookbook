5dd613dd3cf2b63ff6db37d66809c9ae1b0af206
Andrea Campi <andrea.campi@zephirworks.com>
2012-11-11 12:47:40 +0100

    Cleanup: sort dependencies.

--- metadata.rb
+++ metadata.rb
@@ -9,6 +9,6 @@ version          "0.99.0"
   supports os
 end
 
-%w{ postgresql mysql database cloudfoundry }.each do |cb|
+%w{ cloudfoundry database mysql postgresql }.each do |cb|
   depends cb
 end
