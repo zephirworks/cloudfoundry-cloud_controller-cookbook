5978f9cfd83e9d5feb5b091438435f31df1987d0
Andrea Campi <andrea.campi@zephirworks.com>
2012-11-13 00:11:46 +0100

    Fix redis config.

---
+++ attributes/redis.rb
@@ -0,0 +1,6 @@
+
+# Port the vcap_redis server will listen to.
+default['cloudfoundry_cloud_controller']['redis']['port'] = 5454
+
+# Password for clients to connect to the vcap_redis server.
+default['cloudfoundry_cloud_controller']['redis']['password'] = nil
