a34d97af24e646dc8dea5ded7f6274aa6be56a9b
Andrea Campi <andrea.campi@zephirworks.com>
2013-01-07 00:44:39 +0100

    Bump copyright notices.

--- libraries/cloud_controller_database.rb
+++ libraries/cloud_controller_database.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: cloudfoundry-cloud_controller
 # Libraries:: cloud_controller_database
 #
-# Copyright 2012, ZephirWorks
+# Copyright 2012-2013, ZephirWorks
 # Copyright 2012, Trotter Cashion
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
